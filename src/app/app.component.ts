3918d68cb0a5a302f8f5d57f2e1ab552e01c82b1
ishtiyaque.jaffer@iifl.com <ishtiyaque.jaffer>
2024-07-03 11:40:54 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { Deeplinks } from '@awesome-cordova-plugins/deeplinks/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private deeplinks: Deeplinks,
+    private router: Router,
+    private zone: NgZone
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.setupDeeplinks();
+    });
+  }
+
+  setupDeeplinks() {
+    this.deeplinks.route({ '/:slug': 'posts' }).subscribe(
+      match => {
+        console.log('Successfully matched route', match);
+
+        // Create our internal Router path by hand
+        // const internalPath = `/${match.$route}/${match.$args['slug']}`;
+
+        // Run the navigation in the Angular zone
+        this.zone.run(() => {
+          this.router.navigateByUrl('/login');
+        });
+      },
+      nomatch => {
+        // nomatch.$link - the full link data
+        console.error("Got a deeplink that didn't match", nomatch);
+      }
+    );
+  }
 }
